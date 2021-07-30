c111e77b45b7776b90f1a3b3f917f8e7b280c8a4
nothub <48992448+nothub@users.noreply.github.com>
2021-07-30 19:43:37 +0200

    init

---
+++ src/main/java/org/zerobzerot/discordbot/Main.java
@@ -0,0 +1,14 @@
+package org.zerobzerot.discordbot;
+
+import org.tinylog.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logger.debug("debug stuff");
+        Logger.info("Hello World!");
+        Logger.error("small problem");
+        Logger.error("big problem");
+    }
+
+}
