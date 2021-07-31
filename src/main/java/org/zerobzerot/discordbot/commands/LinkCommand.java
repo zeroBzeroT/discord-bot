38a6002344afd1f3c297f5c2691c28feab7e65dc
nothub <48992448+nothub@users.noreply.github.com>
2021-07-31 02:53:36 +0200

    link command

---
+++ src/main/java/org/zerobzerot/discordbot/commands/LinkCommand.java
@@ -0,0 +1,41 @@
+package org.zerobzerot.discordbot.commands;
+
+import cc.neckbeard.utils.UuidConverter;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+
+import java.util.UUID;
+
+public class LinkCommand extends SlashCommand {
+
+    public LinkCommand() {
+        super("link", "link your mc account", Type.PUBLIC);
+        addOption(OptionType.STRING, "uuid", "mc account uuid", true);
+        addOption(OptionType.INTEGER, "pin", "secret pin", true);
+    }
+
+    @Override
+    public void run(SlashCommandEvent event) {
+        OptionMapping uuidOption = event.getOption("uuid");
+        OptionMapping pinOption = event.getOption("pin");
+        // Check if option is present
+        if (uuidOption == null || pinOption == null) {
+            // Send warning reply
+            event.reply("Required option not specified \uD83E\uDD28").setEphemeral(false).queue();
+            return;
+        }
+        final UUID uuid;
+        try {
+            uuid = UuidConverter.of(uuidOption.getAsString());
+        } catch (IllegalArgumentException ex) {
+            event.reply("That was not a valid uuid \uD83E\uDD28").setEphemeral(false).queue();
+        }
+        var pin = (int) pinOption.getAsLong();
+        if (pin < 1 || pin > 9999) {
+            event.reply("That was not a valid pin \uD83E\uDD28").setEphemeral(false).queue();
+        }
+        // TODO: load oldfaggotry data from redis, if uuid found and pin is correct, set role.
+    }
+
+}
